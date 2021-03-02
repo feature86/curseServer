bde2a2a4dc8567f980e197f1857584b02e1170ec
Paul Picher <paul@picher.cc>
2021-03-02 20:53:22 +0100

    add Person

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,12 @@
 import dotenv from 'dotenv';
 import { createConnection } from './db/connection';
 import Curse from './db/model/curse';
+import Person from './db/model/person';
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 import path from 'path';
 import fs from 'fs';
+import { createSha256Hash } from './utils';
 
 const ENVFILE = '.env';
 
@@ -35,6 +38,7 @@ const main = async () => {
 
   const srv = express();
   srv.use(express.json());
+  srv.use(cors());
   srv.get('/', (req: Request, res: Response) => {
     res.json('Hello I am the Curse Api!!');
   });
@@ -43,7 +47,7 @@ const main = async () => {
     console.log(`Application is listening on port ${config.PORT}`);
   });
 
-  srv.post('/curse/create', async (req: Request, res: Response) => {
+  srv.post('/curse', async (req: Request, res: Response) => {
     try {
       await Curse.create({
         person: req.body.person,
@@ -67,7 +71,7 @@ const main = async () => {
     res.json(curseList);
   });
 
-  srv.get('/curse/list/', async (req: Request, res: Response) => {
+  srv.get('/curse', async (req: Request, res: Response) => {
     const curseList = await Curse.find({});
     res.json({ count: curseList.length });
   });
@@ -96,6 +100,45 @@ const main = async () => {
           res.json(e);
         }
       }
+    }
+  });
+
+  srv.get('/person', async (req: Request, res: Response) => {
+    const personList = await Person.find({});
+    res.json(personList);
+  });
+
+  srv.get('/person/:id', async (req: Request, res: Response) => {
+    if (!req.params.id) {
+      res.statusCode = 400;
+      res.json('Bad Request, Param Missing');
+    }
+    const userHash = req.params.id;
+    const person = await Curse.find({ userHash }).exec();
+    res.json(person);
+  });
+
+  srv.post('/person', async (req: Request, res: Response) => {
+    try {
+      const userHash = createSha256Hash(req.body.name.toLowerCase());
+      const search = await Person.find({ userHash });
+      if (search.length === 1) {
+        res.json(search[0]);
+      } else if (search.length > 1) {
+        res.statusCode = 500;
+        res.json({ error: 'More than one user exists' });
+      }
+
+      const person = await Person.create({
+        name: req.body.name,
+        userHash: userHash,
+        image: req.body.image,
+      });
+
+      res.json(person);
+    } catch (e) {
+      res.statusCode = 500;
+      res.json({ error: e });
     }
   });
 
